88d39f4301f3b5bec10631b458b728ebb7d0cd01
Naila Hamida <naila.hamida@gmail.com>
2025-01-13 10:33:19 -0500

    Test Classes

--- Projet 4/force-app/main/default/lwc/opportunityProductsViewer/opportunityProductsViewer.js
+++ Projet 4/force-app/main/default/lwc/opportunityProductsViewer/opportunityProductsViewer.js
@@ -1,36 +1,65 @@
 import { LightningElement, api, wire, track } from 'lwc';
-import getOpportunityProductsbyOpportunityId from '@salesforce/apex/OpportunityProductsViewerController.getOpportunityProductsbyOpportunityId';
+import { refreshApex } from '@salesforce/apex';
 import { NavigationMixin } from 'lightning/navigation';
+import getOpportunityProductsbyOpportunityId from '@salesforce/apex/OpportunityProductsViewerController.getOpportunityProductsbyOpportunityId';
+import getUserProfileName from '@salesforce/apex/OpportunityProductsViewerController.getUserProfileName';
 import deleteOpportunityProduct from '@salesforce/apex/OpportunityProductsViewerController.deleteOpportunityProduct';
+import ProductName from "@salesforce/label/c.LWC_OpportunityProducts_ProductName";
+import Delete from "@salesforce/label/c.LWC_OpportunityProducts_Delete";
+import Quantity from "@salesforce/label/c.LWC_OpportunityProducts_Quantity";
+import QuantityInStock from "@salesforce/label/c.LWC_OpportunityProducts_QuantityInStock";
+import Title from "@salesforce/label/c.LWC_OpportunityProducts_Title";
+import TotalPrice from "@salesforce/label/c.LWC_OpportunityProducts_Total_Price";
+import UnitPrice from "@salesforce/label/c.LWC_OpportunityProducts_UnitPrice";
+import ViewProduct from "@salesforce/label/c.LWC_OpportunityProducts_ViewProduct";
+import QuantityError from "@salesforce/label/c.LWC_OpportunityProducts_QuantityError";
+import NoProduct from "@salesforce/label/c.LWC_OpportunityProducts_NoProduct";
 
-export default class OpportunityProductsViewer extends LightningElement {
+export default class OpportunityProductsViewer extends NavigationMixin(LightningElement) {
     @api recordId;
     @track opportunityProducts = [];
-    
+    datatableTitle = Title;
+    quantityErrorMessage = QuantityError;
+    noProductMessage = NoProduct;
+
     columns = [
-        { label: 'Nom du Produit', fieldName: 'Product2Name', type: 'text' },
-        { label: 'Quantité', fieldName: 'Quantity', type: 'text' },
-        { label: 'Prix unitaire', fieldName: 'UnitPrice', type: 'currency', cellAttributes: { alignment: 'left' } },
-        { label: 'Prix Total', fieldName: 'TotalPrice', type: 'currency', cellAttributes: { alignment: 'left' } },
-        { label: 'Quantité en Stock', fieldName: 'Product2QuantityInStock__c', type: 'text' },
-        { label: 'Supprimer', type: 'button-icon',
+        { label: ProductName, fieldName: 'Product2Name', type: 'text' },
+        { label: Quantity, fieldName: 'Quantity', type: 'text', cellAttributes: {
+            style: { fieldName: 'quantityStyle' }}},
+        { label: UnitPrice, fieldName: 'UnitPrice', type: 'currency', cellAttributes: { alignment: 'left' } },
+        { label: TotalPrice, fieldName: 'TotalPrice', type: 'currency', cellAttributes: { alignment: 'left' } },
+        { label: QuantityInStock, fieldName: 'Product2QuantityInStock__c', type: 'text' },
+        { label: Delete, type: 'button-icon',
             typeAttributes: {
                 label: 'Supprimer',
                 name: 'delete',
                 iconName: 'utility:delete',
                 variant: 'neutral',
+
             }
         },
-        { label: 'Voir produit', type: 'button',
-            typeAttributes: {
-                label: 'View Product',
-                name: 'open',
-                iconName: 'utility:preview',
-                iconPosition: 'left',
-                variant: 'brand',
-            },
-        }
     ];
+
+    profileName;
+
+    @wire(getUserProfileName)
+    wiredProfile({ data }) {
+        if (data) {
+            this.profileName = data;
+        if (this.profileName === 'System Administrator') {
+            this.columns.push({
+                    label: ViewProduct, 
+                    type: 'button', 
+                    typeAttributes: {
+                        label: 'View Product',
+                        name: 'open',
+                        iconName: 'utility:preview',
+                        iconPosition: 'left',
+                        variant: 'brand',
+                    },
+                });
+            }}}
+ 
 
     @wire(getOpportunityProductsbyOpportunityId, { opportunityId: '$recordId' })
     wiredOpportunityLineItems(result) {
@@ -41,9 +70,12 @@ export default class OpportunityProductsViewer extends LightningElement {
                 ...item,
                 Product2Name: item.Product2.Name,
                 Product2QuantityInStock__c: item.Product2.QuantityInStock__c,
+                quantityStyle: item.Insufficient_Quantity_in_stock__c ? 'font-weight: bold; color: red; background: #D3D3D3' : ''  
             }));
+        this.hasInsufficientStock = this.opportunityProducts.some(item => item.Insufficient_Quantity_in_stock__c);
         } else if (result.error) {
             this.opportunityProducts = [];
+            this.hasInsufficientStock = false;
         }
     }
 
@@ -53,20 +85,21 @@ export default class OpportunityProductsViewer extends LightningElement {
 
     handleRowAction(event) {
         const actionName = event.detail.action.name;  
-        const recordId = event.detail.row.Id;  
+        const productId = event.detail.row.Product2Id;  
+        const recordId = event.detail.row.Id; 
     
         if (actionName === 'open') {  
-            this.openRecord(recordId);
+            this.openRecord(productId);
         } else if (actionName === 'delete') {  
-            this.deleteRow(recordId);
+            this.deleteRecord(recordId);
         }
     }
 
-    openRecord(recordId) {
+    openRecord(productId) {
         this[NavigationMixin.Navigate]({
             type: 'standard__recordPage',  
             attributes: {
-                recordId: recordId,   
+                recordId: productId,   
                 objectApiName: 'OpportunityLineItem', 
                 actionName: 'view'  
             }
@@ -74,7 +107,10 @@ export default class OpportunityProductsViewer extends LightningElement {
     }
 
     deleteRecord(recordId) {
-        deleteOpportunityProduct({ opportunityLineItemId: recordId })  
+        deleteOpportunityProduct({ opportunityLineItemId: recordId })
+        .then(() => {
+            return refreshApex(this.wiredResult);})
 
-}
+    }
+
 }
